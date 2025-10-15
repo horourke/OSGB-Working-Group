5227457e618081feebd84447994a5fa8700ac392
Jose Angel Sanchez Gomez <josesa@skylark.ib.hpcc.ucr.edu>
2025-10-15 15:51:27 -0700

    SIMS4: adding micro-run dependent random seeds in simulations

---
+++ rvar_project/experiments_cluster/sims_4/200_gimme/133_ClusterPassExperiment50.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#SBATCH --job-name=mvar5
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=josesa@ucr.edu
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=32gb
+#SBATCH --time=2-00:00:00
+#SBATCH --output=200_gimme/experiments1/logs/output%a.out
+#SBATCH --array=490-729
+
+pwd; hostname; date
+
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo This is task $SLURM_ARRAY_TASK_ID,
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo a
+echo a
+
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo 1 Importing R and Rscript:
+module load R/4.4.2
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo a
+echo a
+
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo 3 Running 200_gimme/103_SimulationScript.R with input $SLURM_ARRAY_TASK_ID 2
+Rscript 200_gimme/103_SimulationScript.R $SLURM_ARRAY_TASK_ID 2
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo a
+echo a
+
+date
+
