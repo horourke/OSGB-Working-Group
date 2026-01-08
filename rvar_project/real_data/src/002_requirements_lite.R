50aeeee4d607a09235a92dfc15029bd9485d2474
jsgomez94 <61295183+jsgomez94@users.noreply.github.com>
2026-01-08 14:52:55 -0800

    realdata: EEG data analysis sample code

---
+++ rvar_project/real_data/src/002_requirements_lite.R
@@ -0,0 +1,22 @@
+
+library(e1071)
+library(magrittr)
+#library(lubridate)
+library(tidyverse)
+#library(lobstr)
+
+## Model fitting packages:
+library(glmnet)
+library(mvtnorm)
+#library(gimme)
+#library(multivar)
+#library(BigVAR)
+
+## Plotting packages:
+#library(ggh4x)
+library(gridExtra)
+library(plot.matrix)
+
+
+## Resetting WD:
+setwd(wd)
