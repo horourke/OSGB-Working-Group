6c784c7dfd94e89b74e5ab91cbe9950e2541166f
horourke <hollyo@ucr.edu>
2026-01-19 14:10:39 -0800

    002_requirements_lite.R for ARI

---
+++ rvar_project/real_data/ARI Sleep Cognitive/002_requirements_lite.R
@@ -0,0 +1,29 @@
+install.packages("e1071")
+install.packages("magrittr")
+install.packages("tidyverse")
+
+library(e1071)
+library(magrittr)
+#library(lubridate)
+library(tidyverse)
+#library(lobstr)
+
+install.packages("glmnet")
+install.packages("mvtnorm")
+
+## Model fitting packages:
+library(glmnet)
+library(mvtnorm)
+#library(gimme)
+#library(multivar)
+#library(BigVAR)
+
+install.packages("gridExtra")
+install.packages("plot.matrix")
+
+## Plotting packages:
+#library(ggh4x)
+library(gridExtra)
+library(plot.matrix)
+
+
