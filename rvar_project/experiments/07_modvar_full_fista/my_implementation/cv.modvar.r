90455e1e7fe368b2dbcb2fcb3dc304e62e6acfff
jsgomez94 <61295183+jsgomez94@users.noreply.github.com>
2026-02-26 11:35:47 -0800

    MV_Fista: new implementation of MOD-VAR through the FISTA algorithm
    The new implementation of our MOD-VAR allows for several improvements.
    1) Implementation with Rccp, which allows for a highly efficient code. It also incorporates several warm-start and calculation shortcuts to enhance performance.
    2) It allows for the fit of multiple subjects with moderator effects and fully idiographic effects, as well as combinations of the two.
    3) It has implementation of multiple CV procedures for model selection, such as the rolling-window cross validation and by-subject  cross-validation.
    4) It contains examples to showcase its use and usefulness.

---
+++ rvar_project/experiments/07_modvar_full_fista/my_implementation/cv.modvar.r
@@ -0,0 +1,337 @@
+
+###########################################################
+###########################################################
+## build_design:
+##  Creates the matrices $W$ and $Y$ to solve the optimization 
+##  problem of MOD-VAR, of the form,
+##    $$ \hat{B} = \argmin_{B} \frac{1}{2N} \|Y - WB\|_F^2 + \lambda_1\|B\|_{1,w}.$$
+##
+##  INPUTS:
+##      Ylist   : list of length n, containing matrices of dimensions 
+##                  $Tk \times d$, with the time series data for 
+##                  each subject.
+##      X       : numeric matrix of dimensions $n x p$. The $k$-th 
+##                  row X[k,] must contain the moderator variables of
+##                  subject $k$, associated with the $k$-th matrix
+##                  in Ylist[[k]].
+##      multi   : if multi = TRUE, we add a fully idiographic
+##                  component to each subject, independent from
+##                  the moderator effects. 
+##
+##  OUTPUTS:
+##      Wmat    : Design matrix of dimension $N\times q$, where 
+##                  $N = \sum_k (T_k-1)$ and $q = d (p+1)$ if 
+##                  multi = FALSE, or $q = d (n+p+1)$ if multi = TRUE.
+##      Ymat    : Response matrix of dimension $N \times d$.
+## 
+build_design <- function(Ylist, X = NULL, multi = FALSE) {
+
+    n <- length(Ylist)
+    d <- ncol(Ylist[[1]])
+    p <- ncol(X)
+
+    ## Response matrix:
+    Yl <- Ylist %>%
+        lapply(function(x) x[-1,])
+    ## Pre-processed predictor matrix:
+    Zl <- Ylist %>%
+        lapply(function(x) {T = nrow(x); return(x[-T,])} )
+    Wl <- list()
+
+    ## Building MOD-VAR predictor matrix W = [W,   X x W,   I x W]
+    
+    for (subj_ind1 in 1:n) {
+        Zfirst  <- Zl[[subj_ind1]]
+        Mzero   <- matrix(0, nrow = nrow(Zfirst), ncol = d)
+        Wl[[subj_ind1]] <- Zfirst
+
+        if (!is.null(X)) {
+            for (mod_ind in 1:p) {
+                Zmod <- X[subj_ind1 , mod_ind] * Zfirst
+                Wl[[subj_ind1]] <- cbind(Wl[[subj_ind1]], Zmod)
+            }
+        }
+        if (multi) {
+            for (subj_ind2 in 1:n) {
+        
+                if(subj_ind1 != subj_ind2) {
+                    Wl[[subj_ind1]] <- cbind(Wl[[subj_ind1]], Mzero)
+                } else {
+                    Wl[[subj_ind1]] <- cbind(Wl[[subj_ind1]], Zfirst)
+                }
+            }
+        }
+    }
+    
+    Wmat <- do.call(rbind, Wl)
+    Ymat <- do.call(rbind, Yl)
+
+    OUTPUT <- list(Wmat = Wmat, Ymat = Ymat)
+    return(OUTPUT)
+}
+
+
+
+
+
+
+
+###########################################################
+###########################################################
+## build_rolling_windows:
+##  Creates the indexes necessary to perform rolling cross
+##  validation for multi-subject data based on Ymat and Wmat.
+##
+##  INPUTS:
+##      Ts  : numeric vector containing the number of time measurements
+##              per subject.  
+##      
+##  OUTPUTS:
+##      cv.train_list   : List. Contains the rows of Wmat and Ymat
+##                          used for training at each rolling window 
+##                          CV-fold.
+##      cv.test_list    : List. Contains the rows of Wmat and Ymat
+##                          used for testing the CV-fold. 
+##      nfolds          : Numeric counting the number of 
+##                          rolling windows.
+##
+build_rolling_windows <- function(Ts) {
+    n <- length(Ts)
+    Tms <- Ts - 1
+    trange  <- min(floor(Ts / 3)) 
+    T1s     <- Tms - trange - 3
+    starts  <- cumsum(c(1, Tms))[-(n+1)]
+
+    cv.train_list <- list()
+    cv.test_list <- list()
+    for (t1 in 0:trange) {
+
+        ## Build rolling window indexes:
+        cv.train_temp <- list()
+        cv.test_temp <- rep(0, n)
+        for (i in 1:n) {
+            cv.train_temp[[i]] <- starts[i]:(starts[i] + T1s[i] + t1)
+            cv.test_temp[i] <- (starts[i] + T1s[i] + t1 + 1)
+        }
+        cv.train_list[[t1+1]] <- unlist(cv.train_temp)
+        cv.test_list[[t1+1]] <- cv.test_temp
+    }
+
+    OUTPUT <- list(
+        cv.train_list = cv.train_list,
+        cv.test_list = cv.test_list,
+        nfolds = length(cv.train_list))
+    return(OUTPUT)
+
+}
+
+
+
+
+
+
+
+###########################################################
+###########################################################
+## build_bysubject_windows:
+##  Creates the indexes necessary to perform by-subject cross
+##  validation for multi-subject data based on Ymat and Wmat.
+##  This procedure divides the folds by subjects, and thus
+##  is only applicable when multi = FALSE.
+##
+##  INPUTS:
+##      Ts      : numeric vector containing the number of time measurements
+##                  per subject.  
+##      nfolds  : number of folds. 
+##      
+##  OUTPUTS:
+##      cv.train_list   : List. Contains the rows of Wmat and Ymat
+##                          used for training at each by-subject
+##                          CV-fold.
+##      cv.test_list    : List. Contains the rows of Wmat and Ymat
+##                          used for testing the CV-fold. 
+##      nfolds          : Numeric counting the number of 
+##                          rolling windows.
+##
+build_bysubject_windows <- function(Ts, nfolds) {
+    n <- length(Ts)
+    Tms <- Ts - 1
+    starts  <- cumsum(c(1, Tms))[-(n+1)]
+    ends  <- cumsum(c(Tms))
+    subj_list <- mapply(
+        function(a,b) return(a:b),
+        a = starts, b = ends)
+    
+    nlen_temp   <- ceiling((n / nfolds))
+    reordering  <- sample(1:(nfolds*nlen_temp))
+    order_temp <- rep(1:nfolds, nlen_temp)[reordering]
+    order_folds <- order_temp[1:n]
+    
+    cv.train_list <- lapply(1:nfolds,
+        function(k) return(as.vector(subj_list[,order_folds != k])))
+    cv.test_list <- lapply(1:nfolds,
+        function(k) return(as.vector(subj_list[,order_folds == k])))
+    
+    OUTPUT <- list(
+        cv.train_list = cv.train_list,
+        cv.test_list = cv.test_list,
+        nfolds = nfolds)
+    return(OUTPUT)
+
+}
+
+
+
+
+
+
+
+###########################################################
+###########################################################
+## cv_rolling.mod_var:
+##  Solver that performs cross validation for the selection
+##  of tuning parameters for our proposed MOD-VAR.
+##  It estimates the CV-MSFE and selects the combination of
+##  tuning parameters that minimizes the error.
+##  Allows for rolling or by-subject cross validation.
+##
+##  INPUTS:
+##      Ylist       : list of length n, containing matrices of dimensions 
+##                      $Tk \times d$, with the time series data for 
+##                      each subject.
+##      X           : numeric matrix of dimensions $n x p$. The $k$-th 
+##                      row X[k,] must contain the moderator variables of
+##                      subject $k$, associated with the $k$-th matrix
+##                      in Ylist[[k]].
+##      lambdas1    : decreasing numeric vector. Penalizes the joint
+##                      structure of the time series.
+##      ratios      : decreasing numeric vector. Contains the ratio of 
+##                      lambda2 / lambda1. Determines the magnitude of 
+##                      of penalty for the moderators and multi-var effects.
+##      multi       : if multi = TRUE, we add a fully idiographic
+##                      component to each subject, independent from
+##                      the moderator effects. 
+##      nfolds      : number of folds. Used only if multi = FALSE and
+##                      cv.type = "bysubject".
+##      
+##  OUTPUTS:
+##      Bopt        : optimal estimation of the model B.
+##      lambda1_opt : optimal value of lambda1 according to the cv procedure.
+##      lambda2_opt : optimal value of lambda2 according to the cv procedure.
+##      CV.MSFE     : matrix of cross validation forecasting error. rows correspond
+##                      to lambdas1 range and columns to ratios)
+##
+cv_rolling.mod_var <- function(
+    Ylist, X, 
+    lambdas1, 
+    ratios, 
+    multi = FALSE,
+    cv.type = c("rolling", "bysubject"), ## "bysubject" only valid if "multi = FALSE"
+    nfolds = NULL                        ## Only needed if multi = FALSE, and cv.type = "bysubject"
+    ) { 
+
+    ## Build design matrix:
+    BD <- build_design(Ylist, X, multi)
+    Wmat <- BD$Wmat
+    Ymat <- BD$Ymat
+
+    n <- length(Ylist)
+    q <- ncol(Wmat)
+    N <- nrow(Ymat)
+    d <- ncol(Ymat)
+
+    ## Building the cross validation indexes:
+    nlam1 <- length(lambdas1)
+    nrat  <- length(ratios)
+    Bcvprev <- array(0, c(q, d, nlam1 * nrat))
+
+    Ts  <- unlist(lapply(Ylist, nrow))
+    cv.windows <- NULL
+    if (cv.type == "rolling") {
+        cv.windows <- build_rolling_windows(Ts)
+    } else if (cv.type == "bysubject") {
+        if (multi) {
+            print("Error: the cv.type \"bysubject\" invalid for multi = TRUE.")
+            return(-1)
+        } else if (is.null(nfolds)) {
+            print("Error: invalid value of nfolds.") ; return(-1)
+        } else {
+            cv.windows <- build_bysubject_windows(Ts, nfolds)
+        }
+    } else {print("Error: invalid value of cv.type.") ; return(-1)}
+
+    ## Calculate CV.MSFE:
+    MSFE <- array(0, c(nlam1, nrat, length(cv.windows[[1]])))
+    pb   <- txtProgressBar(1, length(cv.windows[[1]]), style=3)
+
+    for (ind in seq_along(cv.windows[[1]])) {
+        
+        setTxtProgressBar(pb, ind)
+
+        ## Build cv indexes:
+        cv.train    <- cv.windows$cv.train_list[[ind]]
+        cv.test     <- cv.windows$cv.test_list[[ind]]
+
+        ## Select warm start:
+        Bcvprev <- NULL
+        if (ind == 1) {
+            Bcvprev <- array(0, c(q, d, nlam1 * nrat))
+        } else {
+            Bcvprev <- Bout
+        }
+        
+        ## Fit model:
+        Wmat.cv <- Wmat[cv.train, ]
+        Ymat.cv <- Ymat[cv.train, ]
+
+        Bout  <- weighted_lasso_path(
+            X = Wmat.cv,
+            Y = Ymat.cv,
+            lambda1vec = lambdas1,
+            ratiovec = ratios,
+            max_iter = 2000,
+            Bcvprev = Bcvprev,
+            tol = 1e-8)
+
+        ## Forecasting error:
+        Ymat.test <- Ymat[cv.test, ]
+        Wmat.test <- Wmat[cv.test, ]
+
+        MSFE[, , ind] <- forecast_mse_batch(
+            Xf = Wmat.test,
+            Yf = Ymat.test,
+            Bflat = Bout,
+            nlambda1 = nlam1,
+            nratios = nrat)
+        #print(MSFE[, , ind])
+ 
+    }
+    #print(dim(MSFE))
+    CV.MSFE <- apply(MSFE, c(1,2), mean)
+    #print(dim(CV.MSFE))
+
+    ## Find minimal lambda1, lambda2:
+    opt.inds <- which(CV.MSFE == min(CV.MSFE), arr.ind = TRUE)
+    print(opt.inds)
+
+    lambda1_opt <- lambdas1[opt.inds[1,1]]
+    ratio_opt <- ratios[opt.inds[1,2]]
+
+    ## Fit optimal model:
+    Bopt <- weighted_lasso_path(
+        X = Wmat,
+        Y = Ymat,
+        lambda1vec = lambda1_opt,
+        ratiovec = ratio_opt,
+        max_iter = 2000,
+        Bcvprev = array(0, c(q, d, nlam1 * nrat)),
+        tol = 1e-8)
+
+    OUTPUT <- list(
+        Bopt = Bopt,
+        lambda1_opt = lambda1_opt,
+        lambda2_opt = lambda1_opt * ratio_opt,
+        CV.MSFE = CV.MSFE)
+    
+    return(OUTPUT)
+}
---
+++ rvar_project/experiments/07_modvar_full_fista/my_implementation/matrix_fista.cpp
@@ -0,0 +1,282 @@
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+using namespace arma;
+
+
+/*
+ * soft threshold
+ *
+ * Calculates soft thresholding for a vector of values and a vector
+ * of thresholds.
+ *
+ * Parameters:
+ *   x (vec)                : $q \times q$ matrix corresponding to $X^T X$.
+ *   thresh (zeros<vec>)    : (arma::mat)    : $q \times d$ matrix corresponding to $X^T Y$.
+ *
+ * Returns:
+ *   vec: vector containing the entrywise thresholded values of x.
+ *
+ */
+
+// soft threshold
+inline vec soft_threshold(const vec& x, const vec& thresh) {
+    return sign(x) % max(abs(x) - thresh, zeros<vec>(x.n_elem));
+}
+
+
+
+
+/*
+ * fista_lasso_xtx
+ *
+ * Solves a matrix weighted LASSO optimization problem
+ * via the FISTA algorithm for a single choice of penalty
+ * weights. Uses hot-start to make the calculations more 
+ * efficient.
+ * Instead of inputing X and Y, uses XtX and XtY to reduce
+ * computational costs.
+ *
+ * Parameters:
+ *   XtX (arma::mat)    : $q \times q$ matrix corresponding to $X^T X$.
+ *   XtY (arma::mat)    : $q \times d$ matrix corresponding to $X^T Y$.
+ *   W (arma::mat)      : $q \times d$ matrix of penalty weights.
+ *   L (double)         : Lipschitz constant magnitude, $L = L_{max}(XtX)/N$.
+ *   N (int)            : number of observations.
+ *   Bpre (arma::mat)   : $q \times d$ array of coefficients for hot-start.
+ *   maxiter (int)      : set as 1000.
+ *   tol (double)       : tolerance for convergence.
+ *
+ * Returns:
+ *   arma::mat: matrix containing the solution B for the single optimization
+ *                  problem. 
+ *
+ */
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::export]]
+arma::mat fista_lasso_xtx(
+    const arma::mat& XtX,
+    const arma::mat& XtY,
+    const arma::mat& W,
+    double L,
+    int N,
+    const arma::mat& Bpre,
+    int max_iter = 1000,
+    double tol = 1e-6
+) {
+
+    int q = XtX.n_rows;
+    int d = XtY.n_cols;
+
+    arma::mat B = Bpre;
+
+    for (int j = 0; j < d; j++) {
+
+        vec b = B.col(j);
+        vec z = b;
+        vec w = W.col(j);
+        vec XtY_j = XtY.col(j);
+
+        double t = 1.0;
+
+        for (int iter = 0; iter < max_iter; iter++) {
+
+            vec b_old = b;
+
+            // NEW gradient (1/N scaling)
+            vec grad = (1.0 / N) * (XtX * z - XtY_j);
+
+            b = soft_threshold(
+                    z - grad / L,
+                    w / L
+                );
+
+            if (norm(b - b_old, 2) < tol)
+                break;
+
+            double t_new =
+                (1 + std::sqrt(1 + 4 * t * t)) / 2;
+
+            z = b + ((t - 1) / t_new) * (b - b_old);
+
+            t = t_new;
+        }
+
+        B.col(j) = b;
+    }
+
+    return B;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+ * weighted_lasso_path
+ *
+ * Solves a matrix weighted LASSO optimization problem
+ * via the FISTA algorithm for a range of tuning parameters
+ * lambdas1 and ratios.
+ * It exploits hot-starts to make the optimization more efficient.
+ *
+ * Parameters:
+ *   X (arma::mat)          : $n x q$ design matrix.
+ *   Y (arma::mat)          : $n x d$ response matrix.
+ *   lambda1vec (arma::vec) : choices of main-effect tuning parameters.
+ *   ratiovec (arma::vec)   : choices of ratio between lambda2/lambda1.
+ *   Bcvprev (arma::cube)   : hot-start if available.
+ *   maxiter (int)          : set as 1000.
+ *   tol (double)           : tolerance for convergence.
+ *
+ * Returns:
+ *   arma::mat: matrix containing the solution Bout for the optimization
+ *                  problem, for all combinations of tuning parameters. 
+ *
+ */
+
+// [[Rcpp::export]]
+arma::cube weighted_lasso_path(
+    const arma::mat& X,
+    const arma::mat& Y,
+    const arma::vec& lambda1vec,
+    const arma::vec& ratiovec,
+    const arma::cube& Bcvprev,   // NEW
+    int max_iter = 1000,
+    double tol = 1e-6
+) {
+
+    int N = X.n_rows;
+    int q = X.n_cols;
+    int d = Y.n_cols;
+    
+    arma::mat XtX = X.t() * X;
+    arma::mat XtY = X.t() * Y;
+    double L = (2.0 / N) * eig_sym(XtX).max();
+    
+    int n_lambda1 = lambda1vec.n_elem;
+    int n_ratio   = ratiovec.n_elem;
+
+    arma::cube Bout(q, d, n_lambda1 * n_ratio, fill::zeros);
+    bool use_prev = (accu(abs(Bcvprev)) > 0);
+
+
+    for (int i = 0; i < n_lambda1; i++) {
+
+        double lambda1 = lambda1vec[i];
+
+        arma::mat Bprev_ratio(q, d, fill::zeros);
+
+        for (int j = 0; j < n_ratio; j++) {
+
+            double lambda2 = lambda1 * ratiovec[j];
+
+            // ---- Construct W ----
+            arma::mat W(q, d, fill::zeros);
+
+            // first d rows (d x d block)
+            W.rows(0, d-1).fill(lambda1);
+
+            // remaining (q-d) x d
+            if (q > d)
+                W.rows(d, q-1).fill(lambda2);
+
+
+            // Setting initialization:
+            arma::mat Binit(q, d, fill::zeros);
+            int slice_index = i * n_ratio + j;
+            if (use_prev) {
+                // ---- Use previous CV solution ----
+                Binit = Bcvprev.slice(slice_index);
+            } else if (j > 0) {
+                // ---- Original within-row warm start ----
+                Binit = Bprev_ratio;
+            }
+
+            arma::mat B = fista_lasso_xtx(
+                    XtX, XtY, W, L, N,
+                    Binit,
+                    max_iter,
+                    tol);
+
+            Bout.slice(slice_index) = B;
+            Bprev_ratio = B;
+        }
+    }
+
+    return Bout;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * forecast_mse_batch
+ *
+ * Given design matrix Xf and response matrix Yf, 
+ * as well as parameters Bflat, calculates the 
+ * mean squared forecasting error.
+ *
+ * Parameters:
+ *   Xf (arma::mat)     : $nf x q$ design matrix.
+ *   Yf (arma::mat)     : $nf x d$ response matrix.
+ *   Bflat (arma::cube) : $q x d x (nlambda1 * nratios)$ cube.
+ *                          each $q x d$ corresponds to an estimate.
+ *   nlambda1 (int)     : number of lambda1 parameters fitted.
+ *   nratios (int)      : number of ratios parameters fitted.
+ *
+ * Returns:
+ *   arma::mat: mean squared forecasting error for each combination
+ *                  of lambda1 and ratio. rows correspond to lambda1
+ *                  and columns to ratio.
+ *
+ */
+// [[Rcpp::export]]
+arma::mat forecast_mse_batch(
+    const arma::mat& Xf,
+    const arma::mat& Yf,
+    const arma::cube& Bflat,   // q x d x (nlambda1*nratios)
+    int nlambda1,
+    int nratios
+) {
+
+    int nf = Xf.n_rows;
+
+    arma::mat Sxx = Xf.t() * Xf;   // q x q
+    arma::mat Sxy = Xf.t() * Yf;   // q x d
+    double Ynorm = arma::accu(arma::square(Yf));
+
+    arma::mat mse(nlambda1, nratios);
+
+    int idx = 0;
+
+    for (int l = 0; l < nlambda1; ++l) {
+        for (int r = 0; r < nratios; ++r) {
+            int slice_index = l * nratios + r;
+
+            arma::mat B = Bflat.slice(slice_index);
+
+            double term1 = -2.0 * arma::accu(Sxy % B);
+            double term2 = arma::accu((Sxx * B) % B);
+
+            mse(l, r) = (Ynorm + term1 + term2) / nf;
+
+        }
+    }
+
+    return mse;
+}
+
