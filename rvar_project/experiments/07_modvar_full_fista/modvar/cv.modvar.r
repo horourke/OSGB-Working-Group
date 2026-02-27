9eb3c47b945d02a3d433019f10fef37d6fc8f484
jsgomez94 <61295183+jsgomez94@users.noreply.github.com>
2026-02-27 13:13:37 -0800

    MVFista: implementing BIC training, and improving code shape

---
+++ rvar_project/experiments/07_modvar_full_fista/modvar/cv.modvar.r
@@ -0,0 +1,197 @@
+
+###########################################################
+###########################################################
+## cv.modvar:
+##  Solver that performs cross validation for the selection
+##  of tuning parameters for our proposed MOD-VAR.
+##  It estimates the CV-MSFE and selects the combination of
+##  tuning parameters that minimizes the error.
+##  Allows for rolling or by-subject cross validation.
+##
+##  INPUTS:
+##      Ylist       : list of length n, containing matrices of dimensions 
+##                      $Tk \times d$, with the time series data for 
+##                      each subject.
+##      X           : numeric matrix of dimensions $n x p$. The $k$-th 
+##                      row X[k,] must contain the moderator variables of
+##                      subject $k$, associated with the $k$-th matrix
+##                      in Ylist[[k]].
+##      lambdas1    : decreasing numeric vector. Penalizes the joint
+##                      structure of the time series.
+##      ratios      : decreasing numeric vector. Contains the ratio of 
+##                      lambda2 / lambda1. Determines the magnitude of 
+##                      of penalty for the moderators and multi-var effects.
+##      multi       : if multi = TRUE, we add a fully idiographic
+##                      component to each subject, independent from
+##                      the moderator effects. 
+##      nfolds      : number of folds. Used only if multi = FALSE and
+##                      cv.type = "bysubject".
+##      
+##  OUTPUTS:
+##      Bopt        : optimal estimation of the model B.
+##      lambda1_opt : optimal value of lambda1 according to the cv procedure.
+##      lambda2_opt : optimal value of lambda2 according to the cv procedure.
+##      CV.MSFE     : matrix of cross validation forecasting error. rows correspond
+##                      to lambdas1 range and columns to ratios)
+##
+cv.modvar <- function(
+    Ylist, X, 
+    lambdas1, 
+    ratios, 
+    multi = FALSE,
+    cv.type = c("rolling", "bysubject"), ## "bysubject" only valid if "multi = FALSE"
+    nfolds = NULL                        ## Only needed if multi = FALSE, and cv.type = "bysubject"
+    ) { 
+
+    ############################
+    ## Build design matrix:
+    BD <- build_design(Ylist, X, multi)
+    Wmat <- BD$Wmat
+    Ymat <- BD$Ymat
+
+    n <- length(Ylist)
+    q <- ncol(Wmat)
+    N <- nrow(Ymat)
+    d <- ncol(Ymat)
+    p <- ifelse(is.null(X), 0, ncol(X))
+
+    ############################
+    ## Building the cross validation indexes:
+    nlam1 <- length(lambdas1)
+    nrat  <- length(ratios)
+    Bcvprev <- array(0, c(q, d, nlam1 * nrat))
+
+    Ts  <- unlist(lapply(Ylist, nrow))
+    cv.windows <- NULL
+    if (cv.type == "rolling") {
+        cv.windows <- build_rolling_windows(Ts)
+    } else if (cv.type == "bysubject") {
+        if (multi) {
+            print("Error: the cv.type \"bysubject\" invalid for multi = TRUE.")
+            return(-1)
+        } else if (is.null(nfolds)) {
+            print("Error: invalid value of nfolds.") ; return(-1)
+        } else {
+            cv.windows <- build_bysubject_windows(Ts, nfolds)
+        }
+    } else {print("Error: invalid value of cv.type.") ; return(-1)}
+
+    ############################
+    ## Calculate CV.MSFE:
+    MSFE <- array(0, c(nlam1, nrat, length(cv.windows[[1]])))
+    pb   <- txtProgressBar(1, length(cv.windows[[1]]), style=3)
+
+    for (ind in seq_along(cv.windows[[1]])) {
+        
+        setTxtProgressBar(pb, ind)
+
+        ## Build cv indexes:
+        cv.train    <- cv.windows$cv.train_list[[ind]]
+        cv.test     <- cv.windows$cv.test_list[[ind]]
+
+        ## Select warm start:
+        Bcvprev <- NULL
+        if (ind == 1) {
+            Bcvprev <- array(0, c(q, d, nlam1 * nrat))
+        } else {
+            Bcvprev <- Bout
+        }
+
+        ############################
+        ## Fit model:
+        Wmat.cv <- Wmat[cv.train, ]
+        Ymat.cv <- Ymat[cv.train, ]
+
+        Bout  <- weighted_lasso_path(
+            X = Wmat.cv,
+            Y = Ymat.cv,
+            lambda1vec = lambdas1,
+            ratiovec = ratios,
+            max_iter = 2000,
+            Bcvprev = Bcvprev,
+            tol = 1e-8)
+
+        ############################
+        ## Forecasting error:
+        Ymat.test <- Ymat[cv.test, ]
+        Wmat.test <- Wmat[cv.test, ]
+
+        MSFE[, , ind] <- forecast_mse_batch(
+            Xf = Wmat.test,
+            Yf = Ymat.test,
+            Bflat = Bout,
+            nlambda1 = nlam1,
+            nratios = nrat)
+ 
+    }
+    CV.MSFE <- apply(MSFE, c(1,2), mean)
+
+    ############################
+    ## Find minimal lambda1, lambda2:
+    opt.inds <- which(CV.MSFE == min(CV.MSFE), arr.ind = TRUE)
+    print(opt.inds)
+
+    lambda1_opt <- lambdas1[opt.inds[1,1]]
+    ratio_opt <- ratios[opt.inds[1,2]]
+
+    ############################
+    ## Fit optimal model:
+    Bopt <- weighted_lasso_path(
+        X = Wmat,
+        Y = Ymat,
+        lambda1vec = lambda1_opt,
+        ratiovec = ratio_opt,
+        max_iter = 2000,
+        Bcvprev = array(0, c(q, d, nlam1 * nrat)),
+        tol = 1e-8)
+
+    dim(Bopt) <- c(q,d)
+    Bopt <- t(as.matrix(Bopt))
+
+    ############################
+    ## Build outputs:
+    joint_coeffs <- Bopt[, 1:d]
+    if(p > 0) {
+        moderator_coeffs <- lapply(
+            1:p,
+            function(k) {Bopt[, d * k + 1:d]})
+    } else moderator_coeffs = NULL
+    if (multi) {
+        idiographic_coeffs <- lapply(
+            1:n,
+            function(k) {Bopt[, d *(p + k) + 1:d]})
+    } else idiographic_coeffs = NULL
+
+    ############################
+    ## Build individual graphs:
+    B_total <- list()
+    for (ind in 1:n) {
+
+        B_total[[ind]] <- Bopt[, 1:d]
+
+        if (p > 0) {## Incorporating moderated effects.
+            x <- X[ind, ]
+            for (ent_ind in 1:p) {
+                B_total[[ind]] <- B_total[[ind]] + x[ent_ind] * Bopt[, ent_ind * d + (1:d)]
+            }
+        } 
+        if (multi) {## Incorporating fully idiographic effects.
+            B_total[[ind]] <- B_total[[ind]] + Bopt[, (p + ind) * d + (1:d)]
+        }
+    }
+
+    OUTPUT <- list(
+        lambda1 = lambdas1,
+        ratios = ratios,
+        CV.MSFE = CV.MSFE,
+        lambda1_opt = lambda1_opt,
+        ratio_opt = ratio_opt,
+        lambda2_opt = lambda1_opt * ratio_opt,
+        opt_coeffs = Bopt,
+        joint_coeffs = joint_coeffs,
+        moderator_coeffs = moderator_coeffs,
+        idiographic_coeffs = idiographic_coeffs,
+        bysubject_coeffs = B_total)
+
+    return(OUTPUT)
+}
---
+++ rvar_project/experiments/07_modvar_full_fista/modvar/example_bic.r
@@ -0,0 +1,249 @@
+
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+## Example: estimating fully idiographic structure
+##          without moderators.
+
+library(Rcpp)
+library(RcppArmadillo)
+library(magrittr)
+library(plot.matrix)
+setwd("rvar_project/experiments/07_modvar_full_fista/")
+Rcpp::sourceCpp("modvar/matrix_fista.cpp")
+source("modvar/auxfunct.r")
+source("modvar/bic.modvar.r")
+
+
+
+set.seed(123)
+T  <- 100
+p  <- 2
+d  <- 5
+n  <- 4
+q  <- d * (n + 1)
+
+X <- matrix(runif(n * p, -1, 1), n, p)
+Btrue <- list()
+sigma <- 0.1
+Ylist <- list()
+for(i in 1:n) {
+    # True sparse coefficient matrix
+    Btrue[[i]] <- 0.5 * diag(d)
+    Btrue[[i]][i, i+1] <- -0.5
+    
+    print(Btrue[[i]])
+
+    Ymat <- matrix(0, nrow = T + 100, ncol = d)
+    for(t_ind in 2:(T + 100)) {
+        Ymat[t_ind, ] <- Btrue[[i]] %*% as.vector(Ymat[t_ind - 1, ]) + rnorm(d, sd = sigma)
+    }
+    Ylist[[i]] <- Ymat[101:(T + 100), ]
+}
+lapply(Ylist, dim)
+
+
+
+lambda1vec <- 10^(seq(0, -4, length.out = 25))
+ratiovec   <- 10^(seq(5, -5, length.out = 20))
+Bcvprev <- array(0, c(q,d, 4*5))
+
+res <- bic.modvar(
+    Ylist, X = NULL, lambda1vec, ratiovec, multi = TRUE,
+    alpha = 0.9)
+opt_coeffs <- res$opt_coeffs
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(2,1))
+plot(res$BIC, 10)
+plot(opt_coeffs, breaks = 30)
+res$lambda1_opt
+res$lambda2_opt
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(1,1))
+plot(res$joint_coeffs, breaks = seq(-0.6,0.6, length.out = 30))
+res$moderator_coeffs
+
+par(mfrow = c(3,1)) 
+for(i in 1:3) {
+    plot(res$bysubject_coeffs[[i]], breaks = seq(-1,1, length.out = 30))
+} 
+
+
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+## Example: estimating MOD-VAR with moderators, but no
+##          fully idiographic component.
+
+library(Rcpp)
+library(RcppArmadillo)
+library(magrittr)
+library(plot.matrix)
+setwd("rvar_project/experiments/07_modvar_full_fista/")
+Rcpp::sourceCpp("modvar/matrix_fista.cpp")
+source("modvar/auxfunct.r")
+source("modvar/bic.modvar.r")
+
+set.seed(123)
+
+T  <- 30
+p  <- 2
+d  <- 5
+n  <- 20
+q  <- d * (p + 1)
+
+X <- matrix(runif(n * p, -1, 1), n, p)
+Btrue <- list()
+sigma <- 0.1
+Ylist <- list()
+for(i in 1:n) {
+    # True sparse coefficient matrix
+    Btrue[[i]] <- 0.5 * diag(d)
+    
+    
+    Bx1 <- matrix(0,d,d)
+    Bx2 <- matrix(0,d,d)
+    Bx1[3,1] <- -0.5 * X[i, 1] 
+    Bx2[2,5] <- -0.5 * X[i, 2]
+    
+    Btrue[[i]] <- Btrue[[i]] + Bx1 + Bx2
+    print(i)
+    print(Btrue[[i]])
+
+    Ymat <- matrix(0, nrow = T + 100, ncol = d)
+    for(t_ind in 2:(T + 100)) {
+        Ymat[t_ind, ] <- Btrue[[i]] %*% as.vector(Ymat[t_ind - 1, ]) + rnorm(d, sd = sigma)
+    }
+    Ylist[[i]] <- Ymat[101:(T + 100), ]
+    
+    print(length(Ylist))
+}
+lapply(Ylist, dim)
+
+
+
+lambda1vec <- 10^(seq(0, -6, length.out = 25))
+ratiovec   <- 10^(seq(3, -3, length.out = 20))
+Bcvprev <- array(0, c(q,d, 4*5))
+
+res <- bic.modvar(
+    Ylist, X = X, lambda1vec, ratiovec, multi = FALSE,
+    alpha = 0.9)
+opt_coeffs <- res$opt_coeffs
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(2,1))
+plot(res$BIC, 10)
+plot(opt_coeffs, breaks = 30)
+res$lambda1_opt
+res$lambda2_opt
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(3,1))
+plot(res$joint_coeffs, breaks = seq(-0.6,0.6, length.out = 30))
+for(i in 1:p) {
+    plot(res$moderator_coeffs[[i]], breaks = seq(-1,1, length.out = 30))
+}
+
+par(mfrow = c(3,1)) 
+for(i in 1:3) {
+    plot(res$bysubject_coeffs[[i]], breaks = seq(-1,1, length.out = 30))
+} 
+
+
+
+
+
+
+
+#####################################################
+#####################################################
+#####################################################
+#####################################################
+## Example: estimating MOD-VAR with moderators, and
+##          fully idiographic components.
+
+library(Rcpp)
+library(RcppArmadillo)
+library(magrittr)
+library(plot.matrix)
+setwd("rvar_project/experiments/07_modvar_full_fista/")
+Rcpp::sourceCpp("modvar/matrix_fista.cpp")
+source("modvar/auxfunct.r")
+source("modvar/bic.modvar.r")
+
+set.seed(123)
+
+T  <- 70
+p  <- 2
+d  <- 10
+n  <- 9
+q <- d * (n + p + 1)
+
+X <- matrix(runif(n * p, -1, 1), n, p)
+Btrue <- list()
+sigma <- 0.1
+Ylist <- list()
+for(i in 1:n) {
+    # True sparse coefficient matrix
+    Btrue[[i]] <- 0.5 * diag(d)
+    Btrue[[i]][i, i+1] <- -0.7
+    
+    Bx1 <- matrix(0,d,d)
+    Bx2 <- matrix(0,d,d)
+    Bx1[3,1] <- -0.5 * X[i, 1] 
+    Bx2[2,5] <- -0.5 * X[i, 2]
+    
+    Btrue[[i]] <- Btrue[[i]] + Bx1 + Bx2
+    print(i)
+    print(Btrue[[i]])
+
+    Ymat <- matrix(0, nrow = T + 100, ncol = d)
+    for(t_ind in 2:(T + 100)) {
+        Ymat[t_ind, ] <- Btrue[[i]] %*% as.vector(Ymat[t_ind - 1, ]) + rnorm(d, sd = sigma)
+    }
+    Ylist[[i]] <- Ymat[101:(T + 100), ]
+    
+    print(length(Ylist))
+}
+lapply(Ylist, dim)
+
+
+
+lambda1vec <- 10^(seq(2, -5, length.out = 10))
+ratiovec   <- 10^(seq(3, -3, length.out = 10))
+Bcvprev <- array(0, c(q,d, 4*5))
+
+res <- bic.modvar(
+    Ylist, X = X, lambda1vec, ratiovec, multi = TRUE,
+    alpha = 0.9)
+opt_coeffs <- res$opt_coeffs
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(2,1))
+plot(res$BIC, 10)
+plot(opt_coeffs, breaks = 30)
+res$lambda1_opt
+res$lambda2_opt
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(3,1))
+plot(res$joint_coeffs, breaks = seq(-0.6,0.6, length.out = 30))
+for(i in 1:p) {
+    plot(res$moderator_coeffs[[i]], breaks = seq(-1,1, length.out = 30))
+}
+
+par(mar = c(5.1, 4.1, 4.1, 4.1), mfrow = c(3,1))
+for(i in 1:3) {
+    plot(res$idiographic_coeffs[[i]], breaks = seq(-1,1, length.out = 30))
+}
+
+par(mfrow = c(3,1)) 
+for(i in 1:3) {
+    plot(res$bysubject_coeffs[[i]], breaks = seq(-1,1, length.out = 30))
+} 
+
+
+
+
+
+
+
