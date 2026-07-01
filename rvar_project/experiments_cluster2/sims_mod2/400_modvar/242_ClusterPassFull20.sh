90b3de1926a96b24abf8a84086946bced024ebe9
Jose Angel Sanchez Gomez <josesa@skylark.ib.hpcc.ucr.edu>
2026-06-30 17:05:12 -0700

    EC2: sims_mod2/var400 full simulations

--- rvar_project/experiments_cluster2/sims_mod2/400_modvar/242_ClusterPassFull20.sh
+++ rvar_project/experiments_cluster2/sims_mod2/400_modvar/242_ClusterPassFull20.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=4gb
 #SBATCH --time=2-00:00:00
-#SBATCH --output=400_modvar/outputs1/logs/output%a.out
+#SBATCH --output=400_modvar/outputs2/logs/output%a.out
 #SBATCH --array=370-729
 
 pwd; hostname; date
@@ -25,8 +25,8 @@ echo a
 echo a
 
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
-echo 3 Running 400_modvar/103_SimulationScript.R with input $SLURM_ARRAY_TASK_ID 3
-Rscript 400_modvar/103_SimulationScript.R $SLURM_ARRAY_TASK_ID 3
+echo 3 Running 400_modvar/203_SimulationScript.R with input $SLURM_ARRAY_TASK_ID 3
+Rscript 400_modvar/203_SimulationScript.R $SLURM_ARRAY_TASK_ID 3
 echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 echo a
 echo a
